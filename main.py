0b4b7b99287b5622f48f9c03f07a1d993e48eaa5
wgarnold <wgarnold95@gmail.com>
2025-06-24 09:35:48 +1000

    Made report function

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-from stats import word_count # import word count function
-from stats import char_count
+from curses.ascii import isalpha
+from stats import (
+    word_count,
+    char_count,
+    chars_dict_to_sorted_list,
+)
 def main(): # passes the path to get_book_text function and prints returned text.
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = word_count(text)
     characters = char_count(text)
-    print(f"{num_words} words found in the document")
-    print(characters)
+    char_sorted_list = chars_dict_to_sorted_list(characters)
+    # print(f"{num_words} words found in the document")
+    print_report(book_path, num_words, char_sorted_list)
 
 
 def get_book_text(path): # takes the book path, opens and then reads to a string
     with open(path) as f:
         return f.read()
+
+def print_report(book_path, num_words, char_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in char_sorted_list:
+        if not item["char"].isalpha():
+           continue
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+
+
 
 main()
--- stats.py
+++ stats.py
@@ -15,3 +15,13 @@ def char_count(book_text):
         else:
             char_dict[char] = 1
     return char_dict
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
